61b1ff05609c128d158306e814ecda3c2ffe902f
groupTwoisTheBest <grupon2.mediaoftware@gmail.com>
2026-05-22 16:21:24 -0500

    b

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ templates = Jinja2Templates(directory="templates")
 async def Raiz(request: Request):
     return templates.TemplateResponse(request=request, name="index.html", context={"request": request})
 
-@app.post("/verificar")
-async def Usuario(request: Request,nombre: str =Form(...), password: int = Form(...)):
-    if password == 1234 and nombre == "admin":
-        return RedirectResponse(url="/usuario")
-    
 @app.post("/usuario")
-async def Usuario(request: Request,nombre: str =Form(...), password: str = Form(...)):
-    return templates.TemplateResponse(request=request, name="usuario.html")
+async def Usuario(request: Request,nombre: str =Form(...), password: int = Form(...)):
+    if password == 1234:
+        return templates.TemplateResponse(request=request, name="usuario.html", context={"nombre": nombre})
+    
+
